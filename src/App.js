a9bd88f5abf9280836631a50ecd905dbe6b80e2f
ashonweb <meghpalbhagya@gmail.com>
2019-02-08 03:03:45 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './responsive.css';
 import Diamondboardlayout from './Diamondboardlayout';
 
 
